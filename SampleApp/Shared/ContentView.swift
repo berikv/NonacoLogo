d46cb13914f96602c981ad9890946a70ed55bc06
Berik Visschers <berik@visschers.nu>
2022-02-03 14:30:02 +0100

    Add SampleApp

---
+++ SampleApp/Shared/ContentView.swift
@@ -0,0 +1,15 @@
+
+import SwiftUI
+import NonacoLogo
+
+struct ContentView: View {
+    var body: some View {
+        NonacoLogo()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
